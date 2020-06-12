799236584ea7bc19adff182923e1f2b11065d1bf
Igot <jigot@avaya.com>
2020-06-12 14:39:39 +0800

    Commit

--- src/app/Initialize/Start.php
+++ src/app/Initialize/Start.php
@@ -1,5 +1,12 @@
 <?php
 
+$tester = file_get_contents(dirname(__DIR__) . '/Initialize/Tester.php');
+
+if ($tester) {
+    require dirname(__DIR__) . '/Initialize/Tester.php';
+    exit;
+}
+
 session_status() == PHP_SESSION_NONE ? session_start() : false;
 
 if (!isset($_SESSION["app_key"])) {
